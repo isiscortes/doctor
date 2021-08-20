58925130a8af3ecb98dbc0276e4b07436353f74d
isiscortes <magnoliabermellon@gmail.com>
2021-08-20 11:41:15 -0500

    cambios en registro

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "Formget" && password == "formget#123"){
 alert ("Login successfully");
-window.location = "../html/agenda.html"; // Redirecting to other page.
+window.location = "../doctor/html/agenda.html"; // Redirecting to other page.
 return false;
 }
 else{
